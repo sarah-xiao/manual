ff588537c4a729891a124836bb33718b000407f0
sarah-xiao <wsocket@126.com>
2017-03-16 20:21:05 +0800

    modify hexadd.c

--- hexadd.c
+++ hexadd.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int transer_1(char x)
 {	
@@ -18,12 +19,26 @@ int transer_1(char x)
 	}
 	return y;
 }
-int main(int iagrc, char *cagrv[])
+int transer_2(char cargv[])
 {
-	printf("iagrc = %d\r\n",iagrc);
-	printf("cagrv = %s\r\n",cagrv[1]);
+	if(cargv == NULL || 10 != strlen(cargv)){
+		printf("the cargv wrong");
+		exit(0);
+	}
+	int temp1 = 0, temp2 = 0, temp3 = 0, temp4 = 0;
+	temp1  = transer_1(cargv[9]) + transer_1(cargv[8])*16 ;
+	temp2  = transer_1(cargv[7]) + transer_1(cargv[6])*16 ;
+	temp3  = transer_1(cargv[5]) + transer_1(cargv[4])*16 ;
+	temp4  = transer_1(cargv[3]) + transer_1(cargv[2])*16 ;	
 
-	int y = 0;
-	y = transer_1(*cagrv[1]);
-	printf("y = %d \r\n",y);
+	printf("%d.%d.%d.%d \r\n",temp4,temp3,temp2,temp1);
+}
+int main(int iargc, char *cargv[])
+{
+	if(iargc != 2){
+		fprintf(stderr,"usage: %s<domain nae or dotted-decimal>\n",
+			cargv[0]);
+		exit(0);
+	}
+	transer_2(cargv[1]);		
 }
